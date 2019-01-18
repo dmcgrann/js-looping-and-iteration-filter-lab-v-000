8d902916856a3ba964365cc821f13e58b7d03959
David McGrann <dpmcgrann@gmail.com>
2019-01-18 00:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function findMatching(collection, name) {
   return collection.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
 }
 
-function fuzzyMatch(collection, )
+function fuzzyMatch(collection, name) {
+  
+}
