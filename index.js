9ae7bacabbf158a48d325e8ec3f97ce7019113e1
David McGrann <dpmcgrann@gmail.com>
2019-01-18 00:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function findMatching(collection, name) {
   return collection.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
 }
 
-function fuzzyMatch(collection, nameBegins) {
-  let nameBeginsLength = nameBegins.length;
+function fuzzyMatch(collection, name) {
+  let nameBeginsWith = name.length;
   
-  return collection.filter(function (driver) { return driver.slice(0, nameBeginsLength) === nameBegins;});
+  return collection.filter(function (driver) { return driver.slice(0, nameBeginsWith) === name;});
 }
 
 function matchName(collection, name) {
