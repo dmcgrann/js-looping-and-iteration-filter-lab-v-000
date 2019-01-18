43de49f14ee30699561a374034ff5006e1b25bf1
David McGrann <dpmcgrann@gmail.com>
2019-01-18 00:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function findMatching(collection, name) {
   return collection.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
 }
 
-function fuzzyMatch(collection, name) {
-  
+function fuzzyMatch(collection, nameBegins) {
+  let nameBegins = 
 }
